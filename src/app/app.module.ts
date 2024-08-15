3874d38676a40f7554f0656f8668e5ed96d5cd6f
Bruno Jardon <bjardon97@gmail.com>
2024-08-15 03:57:09 -0600

    config: set up root firebase module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { cert } from 'firebase-admin/app';
+import { FirebaseModule } from 'src/modules/firebase';
 import { ENVIRONMENT_CONFIGURATION_FACTORY, EnvironmentConfig } from './config';
 
 @Module({
@@ -15,6 +17,15 @@ import { ENVIRONMENT_CONFIGURATION_FACTORY, EnvironmentConfig } from './config';
             useFactory: async (config: ConfigService<EnvironmentConfig>) => ({
                 ...config.get('mongo'),
                 retryWrites: true,
+            }),
+        }),
+        FirebaseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService<EnvironmentConfig>) => ({
+                credential: cert(
+                    config.get('firebase.serviceAccount', { infer: true }),
+                ),
             }),
         }),
     ],
