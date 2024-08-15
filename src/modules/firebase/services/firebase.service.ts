b71d9aeabd92349b3e139bb89f3047511c2e27e5
Bruno Jardon <bjardon97@gmail.com>
2024-08-15 03:54:05 -0600

    refactor: expose required firebase-admin services

--- src/modules/firebase/services/firebase.service.ts
+++ src/modules/firebase/services/firebase.service.ts
@@ -1,17 +1,32 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { initializeApp, App } from 'firebase-admin/app';
+import { getAuth, Auth } from 'firebase-admin/auth';
+import { getMessaging, Messaging } from 'firebase-admin/messaging';
 import { FIREBASE_OPTIONS } from '../constants';
 import { FirebaseOptions } from '../interfaces';
 
 @Injectable()
 export class FirebaseService {
     private firebaseApp: App;
+    private firebaseAuth: Auth;
+    private firebaseMessaging: Messaging;
 
     constructor(@Inject(FIREBASE_OPTIONS) options: FirebaseOptions) {
         this.firebaseApp = initializeApp(options);
+
+        this.firebaseAuth = getAuth(this.firebaseApp);
+        this.firebaseMessaging = getMessaging(this.firebaseApp);
     }
 
-    get admin(): App {
+    get app(): App {
         return this.firebaseApp;
+    }
+
+    get auth(): Auth {
+        return this.firebaseAuth;
+    }
+
+    get messaging(): Messaging {
+        return this.firebaseMessaging;
     }
 }
